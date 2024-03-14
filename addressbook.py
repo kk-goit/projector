db554162c68e29f4219c3645479bce70400a1ebd
Vladyslav Strilko <vladyslav.strilko@welltech.com>
2024-03-14 18:20:46 +0200

    Notes support. Fixed dataloss for unhandled exit. Savefile to gitignore.

--- addressbook.py
+++ addressbook.py
@@ -1,8 +1,13 @@
-from collections import UserDict,defaultdict
+from collections import UserDict, defaultdict
 from datetime import datetime
 import re
 
+
 class IncorrectFormatException(Exception):
+    pass
+
+
+class IncorrectNoteIndexErr(Exception):
     pass
 
 
@@ -12,7 +17,7 @@ class Field:
 
     def __str__(self):
         return str(self.value)
-    
+
     def __eq__(self, other):
         return self.value == other
 
@@ -30,35 +35,37 @@ class Phone(Field):
     def validate(self, value: str):
         if len(value) != 10 or not value.isdecimal():
             raise IncorrectFormatException("Phone must have 10 digits")
-        
+
+
 class Address(Field):
     pass
-        
+
+
 class Email(Field):
     def __init__(self, value: str):
         self.validate(value)
         super().__init__(value)
-    
+
     def validate(self, value: str): 
         pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
         if not re.match(pattern, value):
             raise IncorrectFormatException('Incorrect email format')
-            
+
 
 class Birthday(Field):
     def __init__(self, value: str):
         super().__init__(self.parse(value))
-    
-    
+
     def parse(self, value):
         try:
             return datetime.strptime(value, '%d.%m.%Y')
         except ValueError:
             raise IncorrectFormatException(
                 'Birthday must be in format DD.MM.YYYY')
-    
+
     def __str__(self):
         return self.value.strftime("%d.%m.%Y")
+
 
 class Record:
     def __init__(self, name):
@@ -70,31 +77,31 @@ class Record:
 
     def add_birthday(self, date: str):
         self.birthday = Birthday(date)
-        
+
     def remove_birthday(self):
         self.birthday = None
 
     def add_address(self, address: str):
         self.address = Address(address)
-        
+
     def remove_address(self):
         self.address = None
-        
+
     def add_email(self, email: str):
         self.email = Email(email)
-    
+
     def remove_email(self):
         self.email = None
-    
+
     def add_phone(self, phone: str):
         self.phones.append(Phone(phone))
 
-    def find_phone(self, phone: str, default = None):
+    def find_phone(self, phone: str, default=None):
         for itm in self.phones:
             if str(itm) == phone:
                 return itm
         return default
-    
+
     def remove_phone(self, phone: str):
         itm = self.find_phone(phone)
         if itm:
@@ -120,16 +127,104 @@ class Record:
         return res
 
 
+class Note(Field):
+    def __init__(self, value: str, index: int):
+        super().__init__(value)
+        self.index = index
+
+    def __shorten_value(self) -> str:
+        ch_lim = 30
+        if len(self.value) <= ch_lim:
+            return self.value
+        else:
+            return self.value[:ch_lim - 3] + '...'
+
+    def get_index(self) -> int:
+        return self.index
+
+    def get_preview(self) -> str:
+        return self.__shorten_value()
+
+
+class Notes():
+    def __init__(self):
+        self.data: list[Note] = []
+
+    def __get_by_index(self, index: str) -> Note:
+        try:
+            index = int(index)
+            if index > 0:
+                for note in self.data:
+                    if note.get_index() == index:
+                        return note
+                raise KeyError(f"There is no note with an index {index}")
+            else:
+                raise IncorrectNoteIndexErr("Index can't be < 1.")
+        except ValueError:
+            raise IncorrectNoteIndexErr("Index should be an integer.")
+
+
+    def add(self, note: Note):
+        self.data.append(note)
+
+    def delete(self, index: int):
+        note_to_del = self.__get_by_index(index)
+        for i, note in enumerate(self.data):
+            if note == note_to_del:
+                del self.data[i]
+
+    def get_last_index(self) -> int:
+        max_index = 0
+        for note in self.data:
+            note_index = note.get_index()
+            if note_index > max_index:
+                max_index = note_index
+
+        return max_index
+
+    def list(self, data: list[Note] = None):
+        data = data if data else self.data
+        if len(data) > 0:
+            listed = ""
+            for note in data:
+                listed += f"{note.get_index()} - {note.get_preview()}\n"
+            return listed
+        else:
+            print("Unable to locate any notes.")  # TODO; make it better for search
+
+    def search(self, substr: str):
+        matched: list[Note] = []
+        for note in self.data:
+            if substr in str(note):
+                matched.append(note)
+
+        return self.list(matched)
+
+    def show(self, index: str):
+        return str(self.__get_by_index(index))
+
+
 class AddressBook(UserDict[Name, Record]):
+    def __init__(self, initial_data=None):
+        super().__init__(initial_data)
+        self.notes = Notes()
+
     def add_record(self, rec: Record):
         self.data[rec.name] = rec
+
+    def add_note(self, text: str):
+        note_index = self.notes.get_last_index() + 1
+        note = Note(text, note_index)
+        self.notes.add(note)
+
+        return note
 
     def find(self, name: str):
         rec = self.data.get(name)
         if rec is None:
-            raise KeyError(name)
+            raise KeyError(f"Contact with name {name} not found")
         return rec
-    
+
     def delete(self, name: str):
         self.data.pop(name)
 
@@ -159,7 +254,6 @@ class AddressBook(UserDict[Name, Record]):
         for day in week_birthdays.keys():
             res += "{}: {}\n".format(day, ", ".join(week_birthdays[day]))
         return res.rstrip()
-            
+
     def __str__(self):
         return "\n".join(map(str, self.data.values()))
-    
--- cli_bot.py
+++ cli_bot.py
@@ -1,10 +1,13 @@
 import pickle, cmd
+import os
 from addressbook import *
+
 
 def parse_input(user_input):
     cmd, *args = user_input.split()
     cmd = cmd.strip().lower()
     return cmd, *args
+
 
 def input_error(msg):
     def decorator(func):
@@ -14,14 +17,17 @@ def input_error(msg):
             except IncorrectFormatException as err:
                 return err
             except ValueError:
-                return msg 
+                return msg
             except IndexError:
-                return msg 
-            except KeyError as name:
-                return f"Contact with name {name} not found"
-        
+                return msg
+            except KeyError as err:
+                return err
+            except IncorrectNoteIndexErr as err:
+                return err
+
         return inner
     return decorator
+
 
 @input_error("Give me name and phone please.")
 def add_contact(args, contacts: AddressBook):
@@ -31,12 +37,14 @@ def add_contact(args, contacts: AddressBook):
     contacts.add_record(record)
     return "Contact added."
 
+
 @input_error("Give me name and new phone please.")
 def change_contact(args, contacts: AddressBook):
     name, no = args
     rec = contacts.find(name)
     rec.change_phone(no)
     return "Contact updated."
+
 
 @input_error("Give me name and address please.")
 def add_address(args, contacts: AddressBook):
@@ -48,12 +56,14 @@ def add_address(args, contacts: AddressBook):
     rec.add_address(address)
     return "Address added."
 
+
 @input_error("Give me name please.")
 def del_address(args, contacts: AddressBook):
     name = args[0]
     rec = contacts.find(name)
     rec.remove_address()
     return "Address deleted."
+
 
 @input_error("Give me name and birthday please.")
 def add_birthday(args, contacts: AddressBook):
@@ -62,24 +72,58 @@ def add_birthday(args, contacts: AddressBook):
     rec.add_birthday(bd)
     return "Birthday added."
 
+
 @input_error("Give me name please.")
 def show_phone(args, contacts: AddressBook):
     name = args[0]
     return str(contacts.find(name))
+
 
 @input_error("Give me name please.")
 def show_birthday(args, contacts: AddressBook):
     name = args[0]
     rec = contacts.find(name)
     if rec.birthday is None:
-        return "Birthday don't setted."
+        return "Birthday doesn't setted."
     return f"{name}'s birthday is {rec.birthday}"
+
+
+@input_error("")
+def add_note(book: AddressBook):
+    text = input('Note text:')
+    note = book.add_note(text)
+    return f"Added note with index {note.get_index()}"
+
+
+@input_error("")
+def show_note(args, book: AddressBook):
+    index = args[0]
+    return book.notes.show(index)
+
+
+@input_error("")
+def show_notes(args, book: AddressBook):
+    return book.notes.list()
+
+
+@input_error("")
+def search_notes(args, book: AddressBook):
+    search_str = args[0]
+    return book.notes.search(search_str)
+
+
+@input_error("")
+def del_note(args, book: AddressBook):
+    index = args[0]
+    book.notes.delete(index)
+    return f"Note with index {index} deleted"
+
 
 class PDP11Bot(cmd.Cmd):
     intro = "Welcome to the assistant bot!\n"
     prompt = "(pdp-11) "
     fn = "address-book.dmp"
-    book = AddressBook()
+    book = None
 
     # ---- commands ----
     def do_hello(self, arg):
@@ -89,20 +133,22 @@ class PDP11Bot(cmd.Cmd):
     def do_exit(self, arg):
         "Stop work and good bye"
         print("Good bye!")
-        self.close_book()
+        self.save_book()
         return True
-    
+
     def do_close(self, arg):
         "Stop work and good bye"
         return self.do_exit(arg)
-    
+
     def do_add(self, arg):
         "Adding new contact with phone"
         print(add_contact(self.parse_input(arg), self.book))
+        self.save_book()
 
     def do_change(self, arg):
         "Change the contact phone"
         print(change_contact(self.parse_input(arg), self.book))
+        self.save_book()
 
     def do_phone(self, arg):
         "Show contacts phones"
@@ -119,37 +165,49 @@ class PDP11Bot(cmd.Cmd):
     def do_add_birthday(self, arg):
         "Add/Change birthday for the contact"
         print(add_birthday(self.parse_input(arg), self.book))
+        self.save_book()
 
     def do_show_birthday(self, arg):
         "Show birthday fo the contact"
         print(show_birthday(self.parse_input(arg), self.book))
-        
+
     def do_add_address(self, arg):
         "Add address for the contact"
         print(add_address(self.parse_input(arg), self.book))
-        
+        self.save_book()
+
     def do_delete_address(self, arg):
         "Delete the contact address"
         print(del_address(self.parse_input(arg), self.book))
+        self.save_book()
 
-    # ---- preprocessors ----
-    def preloop(self):
-        with open(self.fn, "rb") as fh:
-            self.book = pickle.load(fh)
+    # ---- note commands ----
+    def do_add_note(self, arg):
+        print(add_note(self.book))
+        self.save_book()
 
-    def precmd(self, line: str) -> str:
-        "Lowering entered commands"
-        words = line.split()
-        if len(words) > 0:
-            words[0] = words[0].lower()
-        return super().precmd(' '.join(words))
-    
-    def completenames(self, text: str, *ignored) -> list[str]:
-        "Lowering inputed command's chars"
-        return super().completenames(text.lower(), *ignored)
-    
+    def help_add_note(self):
+        print("Adds a new note")
+
+    def do_show_note(self, arg):
+        "Shows note by provided index"
+        print(show_note(self.parse_input(arg), self.book))
+
+    def do_show_notes(self, arg):
+        "Shows all notes"
+        print(show_notes(self.parse_input(arg), self.book))
+
+    def do_search_notes(self, arg):
+        "Searches for a note which contains a certain string"
+        print(search_notes(self.parse_input(arg), self.book))
+
+    def do_del_note(self, arg):
+        "Deletes note by provided index"
+        print(del_note(self.parse_input(arg), self.book))
+        self.save_book()
+
     # ---- internal logic ----
-    def close_book(self):
+    def save_book(self):
         "Dump AdressBook to file"
         with open(self.fn, "wb") as fh:
             pickle.dump(self.book, fh)
@@ -157,6 +215,31 @@ class PDP11Bot(cmd.Cmd):
     def parse_input(self, arg: str):
         "Parse input line as tuple"
         return arg.split()
+
+    def open_address_book(self):
+        "Loading the adress book from file if exists"
+        self.book = AddressBook()
+        try:
+            with open(self.fn, "rb") as file:
+                self.book = pickle.load(file)
+        except FileNotFoundError:
+            pass
+
+    # ---- preprocessors ----
+    def preloop(self):
+        "Init data before starting command prompt loop"
+        self.open_address_book()
+
+    def precmd(self, line: str) -> str:
+        "Lowering entered commands"
+        words = line.split()
+        if len(words) > 0:
+            words[0] = words[0].lower()
+        return super().precmd(' '.join(words))
+
+    def completenames(self, text: str, *ignored) -> list[str]:
+        "Lowering inputed command's chars"
+        return super().completenames(text.lower(), *ignored)
 
 
 if __name__ == "__main__":
