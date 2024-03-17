23dc5b41e71672a6b6ef9855a4eeed4d99efa210
Kostiantyn Kolomiichuk <kkondor@yahoo.com>
2024-03-17 20:00:30 +0200

    Add wellcome animation for module

--- santashelper/elf.py
+++ santashelper/elf.py
@@ -1,8 +1,10 @@
 from .classes.SantasHelper import *
+from .classes.animation import welcome_animation
 
 def main():
+    welcome_animation()
     SantasHelper().cmdloop()
-    
+
 if __name__ == "__main__":
     main()
-    
+
--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='santashelper',
-    version='0.9.17',
+    version='0.9.19',
     description="console Santa's helper elf",
     url='https://github.com/kk-goit/projector',
     author='joBounty',
     author_email='kkondor@yahoo.com',
     license='MIT',
     packages=find_namespace_packages(),
-    install_requires=[],
+    install_requires=['asciimatics'],
+    include_package_data=True,
     entry_points={'console_scripts': ['elf = santashelper.elf:main']}
 ) 
