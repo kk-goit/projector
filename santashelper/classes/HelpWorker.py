df4de815fc45098d5614ab6ecdc71849b77d999b
Kostiantyn Kolomiichuk <kkondor@yahoo.com>
2024-03-18 11:49:36 +0200

    fixed merge error - added missing execpt

--- santashelper/classes/HelpWorker.py
+++ santashelper/classes/HelpWorker.py
@@ -14,6 +14,7 @@ def input_error(msg):
                 IncorrectFormatException,
                 IncorrectNoteIndexError,
                 NotesNotFoundError,
+                NotFoundError,
                 KeyError,
             ) as err:
                 return err
